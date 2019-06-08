3e2b9f9273cce133d807c7e9171d8e88e9d1f9c8
IgorSSK <igorsouza.96@hotmail.com>
2019-06-08 09:59:58 -0300

    WebPage Login implemented

--- web/appschool/src/components/App.js
+++ web/appschool/src/components/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import Button from './Button'
+import '../styles/offcanvas.css'
+import logo_title from '../images/logoText.png'
+import logo_img from '../images/logoSchool.png'
+
 const App = () => {
   return (
-    <div className="App">
-      <Button btnText='Acesse sua Turma' />
+    <div>
+      <nav className='navbar navbar-expand-lg fixed-top navbar-dark bg-dark'>
+            <a href='#'>
+                <img className='logo-image' src={ logo_img } alt='Logo Image'/>
+                <img className='logo-text' src={ logo_title } alt='Logo Text'/>
+            </a>
+            <p className='mr-auto headerTitle'>School App</p>
+            <button className='navbar-toggler p-0 border-0' type='button' data-toggle='offcanvas'>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+        </nav>
+        <div className='navbar-collapse offcanvas-collapse border-right' id='navbarsExampleDefault'>
+            <div className='list-group list-group-flush'>
+                    <a href='#' className='list-group-item list-group-item-action bg-dark text-white'>Dashboard</a>
+                    <a href='#' className='list-group-item list-group-item-action bg-dark text-white'>Shortcuts</a>
+                    <a href='#' className='list-group-item list-group-item-action bg-dark text-white'>Overview</a>
+                    <a href='#' className='list-group-item list-group-item-action bg-dark text-white'>Events</a>
+                    <a href='#' className='list-group-item list-group-item-action bg-dark text-white'>Profile</a>
+                    <a href='#' className='list-group-item list-group-item-action bg-dark text-white'>Status</a>
+            </div>
+        </div>
     </div>
   );
 }
--- web/appschool/src/index.js
+++ web/appschool/src/index.js
@@ -1,9 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import './styles/app.scss';
+import './styles/login.css'
+import { Router, Route, browserHistory } from 'react-router'
+import { createBrowserHistory } from "history"
+import App from './components/App';
+import Login from './components/Login';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+(<Router history={ createBrowserHistory() }>
+    <Route path='/' component={ Login }/>
+    <Route path='/home' component={ App }/>
+</Router>)
+, document.getElementById('root'));
 
 serviceWorker.unregister();
